ce9d0199cfbb92f4306ceb796de2402e9a81ae47
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-07 11:57:06 -0400

    created a movie objcet with name and id

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const app = {
     init(formSelector) {
+        this.max = 0
         document
             .querySelector(formSelector)
-            .addEventListener('submit', this.addMovie)
+            .addEventListener('submit', this.addMovie.bind(this))
     },
 
     addMovie(ev) {
         ev.preventDefault()
 
-        const movieName = ev.target.movieName.value
+        const f = ev.target
+
+        const movie = {
+            id: this.max + 1,
+            name: f.movieName.value,
+        }
+        this.max++
     },
 }
 
