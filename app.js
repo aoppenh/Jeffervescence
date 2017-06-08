4ac7d5f3cccc548bb14753b383c5a265bf57212f
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-08 11:25:49 -0400

    added for-loop to remove arrays

--- app.js
+++ app.js
@@ -161,6 +161,13 @@ const app = {
 
             if (clicked_id === this.movies[j].del.id) {
                 $(nm).remove()
+
+                for (let i = 0; i < this.movies.length; i++) {
+                    const nd = '#el' + this.movies[i].id
+                    if (nm === nd) {
+                        this.movies.splice(i, 1)
+                    }
+                }
             }
         }
     },
